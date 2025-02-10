4b9267273f645ba61f3c61a1af828f9c1b7ffae6
Iconic_slayer_04 <aniketmaurya2804@gmail.com>
2025-02-10 22:10:12 +0530

    Add files via upload

---
+++ Heuristic/operators.py
@@ -0,0 +1,52 @@
+#  Assignment operators
+
+
+a = 10 
+b = 15
+a+= 15
+b-= 10
+a*= 10
+b/= 10
+a%= 1
+b//= 15
+a**= 3
+print(a)
+print(b)
+
+
+# Comparison operators
+
+
+c = 25
+d = 15
+c>= 10
+d<= 10
+c== 10
+c!= 10
+d!= 15
+print(c)
+print(d)
+
+
+# Logical operators
+
+
+e = 5
+f = 7
+print(e < 10 and e < 10)
+print(f< 10 or f > 10)
+print(not(f< 10 or f > 10))
+
+
+# Arithematic operators
+
+
+g = 10
+h = 10
+print(g+h)
+print(g-h)
+print(g/h)
+print(g*h)
+print(g%h)
+print(g**h)
+print(g//h)
